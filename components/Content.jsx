b50e2e3b7407d6f0ec332e37667f036b38dffa6c
fishnal <vpatel14@terpmail.umd.edu>
2019-11-07 15:29:56 -0500

    Added simple gallery
    images.json holds an array of paths to full-sized images.

--- components/Content.jsx
+++ components/Content.jsx
@@ -1,14 +1,47 @@
+/**
+ * @typedef ImageGalleryItem
+ * @property {string} original path to original, full-size photo
+ * @property {string} thumbnail path to thumbnail of photo
+ */
+
 import React from 'react';
-import data from '../public/content.json';
+import ImageGallery from 'react-image-gallery';
+/**
+ * @type {string[]}
+ */
+import contentData from '../public/content.json';
+/**
+ * @type {ImageGalleryItem[]}
+ */
+import imageData from '../public/images.json';
+
 export default class Content extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { paragraphs: data };
+
+    imageData.forEach((imgFileObj, i) => {
+      if (!imgFileObj.original) {
+        throw new Error(`Image ${i} does not have an original path`);
+      }
+
+      imgFileObj.originalClass = imgFileObj.originalClass || 'gallery-img';
+
+      imgFileObj.thumbnail = imgFileObj.thumbnail || imgFileObj.original;
+      imgFileObj.thumbnailClass = imgFileObj.thumbnailClass || 'gallery-thumb';
+    });
+
+    this.state = { paragraphs: contentData };
   }
 
   render() {
     return (
       <div id='article-content'>
+        <ImageGallery
+          items={imageData}
+          lazyLoad={true}
+          showPlayButton={false}
+          showFullscreenButton={false}
+        />
         {this.state.paragraphs.map((para, idx) => (
           <p key={`content-paragraph-${idx}`} className='article-paragraph'>
             {para}
