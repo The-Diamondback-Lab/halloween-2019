9e7caa5b12ed11cc545bcc6cf08d0d7c2b72c2f3
fishnal <vpatel14@terpmail.umd.edu>
2019-11-08 13:40:11 -0500

    Extracted credits into a json file

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import credits from '../public/credits.json';
 
 export default class Footer extends React.Component {
   constructor(props) {
@@ -8,9 +9,8 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer>
-        <p className="credits">CREDITS</p>
-        <p>REPORTING: Hadron Chaudhary, Christine Condon, Arya Hodjat and Jillian Atelsek</p>
-        <p>Website Design: Vishal Patel</p>
+        <p className="credits">Credits</p>
+        { credits.map((creditLine, idx) => <p key={`credit-${idx}`}>{creditLine}</p>) }
         <a href="http://dbknews.com" target="_blank">
           <img className="footer-logo" src="/images/dbk-logo.png"></img>
         </a>
