d5d6661a490c4f7a7bc39aa9e1c7adb8d76ad249
fishnal <vpatel14@terpmail.umd.edu>
2019-11-07 21:29:58 -0500

    Updated header content, removed logo and blurb
    Replaced logo with a title
    Replaced blurb with a subtitle

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import '../styles/main.css';
-import data from '../public/data-header.json';
 
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
     console.log(this.props);
-
-    this.state = { data };
   }
 
   render() {
     return (
       <div className='header-bg'>
         <div className='header-content'>
-          <a href='https://dbknews.com' target='_blank'>
-            <img id='header-logo' src='/images/dbk-logo.png'></img>
-          </a>
-          <div id='title-text'>{this.state.data.title}</div>
-          <div id='blurb'>{this.state.data.blurb}</div>
+          <div id="title-text">Little Bus of Horrors</div>
+          <div id="subtitle-text">Halloween 2019 at UMD</div>
         </div>
       </div>
     );
