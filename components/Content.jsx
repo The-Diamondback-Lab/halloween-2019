fe2173baceb01faf41203825cfe7725b54067a14
fishnal <vpatel14@terpmail.umd.edu>
2019-11-08 00:45:35 -0500

    Added gallery directives in content.json
    Directive indicates where a gallery should appear in the content
    Total directive count helps split up images amongst galleries

--- components/Content.jsx
+++ components/Content.jsx
@@ -22,35 +22,92 @@ export default class Content extends React.Component {
       }
     });
 
+    this.splitImages = this.splitImages.bind(this);
     this.state = {
+      /**
+       * @type {string[]}
+       */
       paragraphs: contentData,
-      images
+      /**
+       * @type {string[]}
+       */
+      images: images
     };
   }
 
+  /**
+   * Splits this component's state images into sets.
+   * @param {number} count How many sets to split the images into
+   * @returns {string[][]}
+   */
+  splitImages(count) {
+    let { images } = this.state;
+
+    let sets = [];
+    let start = 0;
+    let imageCount = Math.ceil(images.length / count);
+
+    for (let i = 0; i < count; i++) {
+      sets.push(images.slice(start, start + imageCount));
+      start += imageCount;
+    }
+
+    return sets;
+  }
+
   render() {
-    let imageGallery = (
-      <div className="gallery-wrapper">
+    let { paragraphs } = this.state;
+
+    let galleryData = paragraphs.reduce((data, para, idx) => {
+      if (para === 'GALLERY::') {
+        data.indices.push([idx, data.count]);
+        data.count++;
+      }
+
+      return data;
+    }, {
+      /**
+       * How many gallery indicators/directives were found in the paragraphs list
+       */
+      count: 0,
+      /**
+       * @type number[][]
+       * Maps the index of a gallery directive from `paragraphs` to which directive
+       * count it was (i.e. `[15,1]` indicates directive at index 15 corresponds
+       * to the second gallery, zero-indexed)
+       */
+      indices: []
+    });
+
+    let imageSets = this.splitImages(galleryData.count);
+
+    let galleries = imageSets.map((set, idx) => (
+      <div key={`gallery-${idx}`} className="gallery-wrapper">
         <ImageGallery
-          items={this.state.images}
+          items={set}
           lazyLoad={true}
           showPlayButton={false}
           showFullscreenButton={false}
           showIndex={true}
         />
       </div>
-    );
+    ));
 
-    let articleContent = this.state.paragraphs.map((para, idx) => {
+    let articleContent = paragraphs.map((para, idx) => {
+      // Find the gallery index map for this index (if any)
+      let galleryIndicesMap = galleryData.indices.find(arr => arr[0] === idx);
+
+      if (galleryIndicesMap) {
+        // Found one, return the gallery at index galleryIndicesMap[1]
+        return galleries[galleryIndicesMap[1]];
+      }
+
       if (para.match(/^PERSON\:\:/)) {
         para = `<i>${para.split('::')[1]}</i>`;
       }
 
       return (<p key={`content-paragraph-${idx}`} className='article-paragraph' dangerouslySetInnerHTML={{__html: sanitizeHtml(para)}}></p>);
     });
-
-    let insertIndex = Math.max(articleContent.length / 2 - 1, 0);
-    articleContent.splice(insertIndex, 0, imageGallery);
 
     return (
       <div id='article-content'>
