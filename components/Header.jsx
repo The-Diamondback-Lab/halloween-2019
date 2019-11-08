ee51ec774b16686dff5783d2b6de2c4bd7068083
fishnal <vpatel14@terpmail.umd.edu>
2019-11-07 23:14:12 -0500

    Changed subtitle to lowercase

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,7 +11,7 @@ export default class Header extends React.Component {
     return (
       <div className='header-bg'>
         <div className='header-content'>
-          <div id="title-text">Little Bus of Horrors</div>
+          <div id="title-text">Little Bus of Horrors:</div>
           <div id="subtitle-text">Halloween 2019 at UMD</div>
         </div>
       </div>
