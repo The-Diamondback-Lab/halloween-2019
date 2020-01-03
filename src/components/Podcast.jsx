e9e25783f04cf6490270524a3b46ef360159b45d
fishnal <vpatel14@terpmail.umd.edu>
2020-01-03 03:55:24 -0500

    Fixed typo

--- src/components/Podcast.jsx
+++ src/components/Podcast.jsx
@@ -7,7 +7,7 @@ export default class Podcast extends React.Component {
         width='100%'
         height='166px'
         src={this.props.src}
-        title={Buffer.from(this.props.src).tostring('base64')}
+        title={Buffer.from(this.props.src).toString('base64')}
         scrolling='no'
         frameBorder='no'
         allow='autoplay'
